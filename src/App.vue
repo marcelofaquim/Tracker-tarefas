8aa3e9f99808e1bf1b4aca3541e6a730d60d889c
Marcelo faquim D. Anjos <marcelofaquim0@gmail.com>
2024-04-03 18:53:30 -0300

    primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <BarraLateral />
+
+    </div>
+    <div class="classcolumn is-three-quarter">
+
+    </div>
+
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+import BarraLateral from './components/BarraLateral.vue'
+
+export default defineComponent ({
+  name: 'App',
+  components: {
+    BarraLateral
+  }
+  
+});
+</script>
+
+<style>
+
+</style>
---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,39 @@
+<template>  
+
+    <header>
+        <h1>
+            <img src="../assets/Logo marca Marcelo.png" alt="">
+        </h1>
+    </header>
+
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from 'vue';
+
+export default defineComponent ({
+    name: 'BarraLateral'
+
+})
+
+</script>
+
+<!-- todo o estilo do componete esta abaixo  -->
+
+<style scoped>
+
+header {
+    background: aqua;
+    width: 100%;
+    height: 100vh;
+}
+
+@media only screen and (max-widht: 768px) {
+    header {
+        padding: 2.5em;
+        height: auto;
+    }    
+}
+
+</style>
